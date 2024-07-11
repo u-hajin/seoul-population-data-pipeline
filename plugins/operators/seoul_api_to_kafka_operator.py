567350734c8783f78377f8c5f0e6cd1e96bb5492
YooHaJin <yoohajin.dev@gmail.com>
2024-07-11 17:55:48 +0900

    feat: API를 호출해 kafka publish하는 custom operator

--- plugins/operators/seoul_api_to_kafka_operator.py
+++ plugins/operators/seoul_api_to_kafka_operator.py
@@ -1,96 +1,89 @@
-from airflow.hooks.base import BaseHook
-from airflow.models.baseoperator import BaseOperator
-from confluent_kafka import SerializingProducer
-
-import configparser
 import json
 
-config = configparser.ConfigParser()
-config.read('../../resources/config.ini')
+from airflow.hooks.base import BaseHook
+from airflow.models.baseoperator import BaseOperator
+from airflow.providers.apache.kafka.hooks.produce import KafkaProducerHook
 
 
-def _get_area_code():
-    area_code = config['API']['AreaCode'].split(',')
-    return area_code
+class SeoulApiToKafkaOperator(BaseOperator):
+    template_fields = (
+        'endpoint', 'kafka_config_id', 'topic', 'kafka_publish_key', 'dataset_name', 'area_code', 'current_time')
 
+    def __init__(self, dataset_name, kafka_config_id, topic, kafka_publish_key, current_time=None, **kwargs):
+        super().__init__(**kwargs)
+        self.dataset_name = dataset_name
+        self.http_conn_id = 'openapi.seoul.go.kr'
+        self.endpoint = '{{var.value.apikey_openapi_seoul}}/json/' + self.dataset_name + '/1/5'
+        self.kafka_config_id = kafka_config_id
+        self.topic = topic
+        self.kafka_publish_key = kafka_publish_key
+        self.current_time = current_time
+        self.area_code = '{{var.value.area_code}}'
 
-def _extract_data(date, contents):
-    data = {
-        'current_time': date,
-        'area_name': contents['AREA_NM'],
-        'area_code': contents['AREA_CD'],
-        # 'live_population': contents['LIVE_PPLTN_STTS'],
-        'area_congest_level': contents['AREA_CONGEST_LVL'],
-        'area_congest_message': contents['AREA_CONGEST_MSG'],
-        'area_population_min': contents['AREA_PPLTN_MIN'],
-        'area_population_max': contents['AREA_PPLTN_MAX'],
-        'male_population_rate': contents['MALE_PPLTN_RATE'],
-        'female_population_rate': contents['FEMALE_PPLTN_RATE'],
-        'population_rate_0': contents['PPLTN_RATE_0'],
-        'population_rate_10': contents['PPLTN_RATE_10'],
-        'population_rate_20': contents['PPLTN_RATE_20'],
-        'population_rate_30': contents['PPLTN_RATE_30'],
-        'population_rate_40': contents['PPLTN_RATE_40'],
-        'population_rate_50': contents['PPLTN_RATE_50'],
-        'population_rate_60': contents['PPLTN_RATE_60'],
-        'population_rate_70': contents['PPLTN_RATE_70'],
-        'resident_population_rate': contents['RESNT_PPLTN_RATE'],
-        'non_resident_population_rate': contents['NON_RESNT_PPLTN_RATE']
-    }
+    def execute(self, context):
+        connection = BaseHook.get_connection(self.http_conn_id)
+        base_url = f'http://{connection.host}:{connection.port}/{self.endpoint}'
 
-    return data
+        producer = KafkaProducerHook(kafka_config_id=self.kafka_config_id).get_producer()
 
+        for content in self._call_api(base_url):
+            data = self._extract_data(content)
+            self._kafka_publish(producer, data)
 
-def _call_api():
-    import requests
+        producer.flush()
 
-    headers = {'Content-Type': 'application/json',
-               'charset': 'utf-8',
-               'Accept': '*/*'}
+    def _call_api(self, base_url):
+        import requests
 
-    area_code = _get_area_code()
-    api_key = config['API']['ApiKey']
+        self.area_code = self.area_code.split(',')
 
-    for code in area_code:
-        request_url = f'http://openapi.seoul.go.kr:8088/{api_key}/json/citydata_ppltn/1/5/{code}'
-        response = requests.get(request_url, headers=headers)
-        contents = json.loads(response.text)['SeoulRtd.citydata_ppltn'][0]
+        for code in self.area_code:
+            request_url = f'{base_url}/{code}'
+            response = requests.get(request_url)
+            contents = json.loads(response.text)['SeoulRtd.citydata_ppltn'][0]
 
-        yield contents
+            yield contents
 
+    def _extract_data(self, content):
+        extracted_data = {
+            'area_name': content['AREA_NM'],
+            'area_code': content['AREA_CD'],
+            # 'live_population': content['LIVE_PPLTN_STTS'],
+            'area_congest_level': content['AREA_CONGEST_LVL'],
+            'area_congest_message': content['AREA_CONGEST_MSG'],
+            'area_population_min': content['AREA_PPLTN_MIN'],
+            'area_population_max': content['AREA_PPLTN_MAX'],
+            'male_population_rate': content['MALE_PPLTN_RATE'],
+            'female_population_rate': content['FEMALE_PPLTN_RATE'],
+            'population_rate_0': content['PPLTN_RATE_0'],
+            'population_rate_10': content['PPLTN_RATE_10'],
+            'population_rate_20': content['PPLTN_RATE_20'],
+            'population_rate_30': content['PPLTN_RATE_30'],
+            'population_rate_40': content['PPLTN_RATE_40'],
+            'population_rate_50': content['PPLTN_RATE_50'],
+            'population_rate_60': content['PPLTN_RATE_60'],
+            'population_rate_70': content['PPLTN_RATE_70'],
+            'resident_population_rate': content['RESNT_PPLTN_RATE'],
+            'non_resident_population_rate': content['NON_RESNT_PPLTN_RATE']
+        }
 
-producer = SerializingProducer({
-    'bootstrap.servers': config['KAFKA']['BootstrapServer']
-})
-topic = config['KAFKA']['Topic']
+        if self.current_time is not None:
+            data = {'current_time': self.current_time}
+            data.update(extracted_data)
+            return data
+        else:
+            return extracted_data
 
+    def _delivery_report(self, error, msg):
+        if error is not None:
+            self.log.error(f'Message delivery failed: {error}')
+        else:
+            self.log.info(f'Message delivered to {msg.topic()} [{msg.partition()}]')
 
-def _delivery_report(error, msg):
-    if error is not None:
-        print(f'Message delivery failed: {error}')
-    else:
-        print(f'Message delivered to {msg.topic()} [{msg.partition()}]')
-
-
-def _kafka_publish(producer, topic, data):
-    producer.produce(
-        topic=topic,
-        key=data['area_code'],
-        value=json.dumps(data, ensure_ascii=False),
-        on_delivery=_delivery_report
-    )
-
-    producer.poll(0)
-
-
-if __name__ == '__main__':
-    from datetime import datetime, timezone, timedelta
-
-    KST = timezone(timedelta(hours=9))
-    date = datetime.now(KST).strftime('%Y-%m-%d %H:%M')
-
-    for content in _call_api():
-        data = _extract_data(date, content)
-        _kafka_publish(producer, topic, data)
-
-    producer.flush()
+    def _kafka_publish(self, producer, data):
+        producer.produce(
+            self.topic,
+            key=data[self.kafka_publish_key],
+            value=json.dumps(data, ensure_ascii=False),
+            on_delivery=self._delivery_report)
+        producer.poll(0)
