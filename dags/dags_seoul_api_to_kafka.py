d26cd340e47e0b1a9c06f9a05327840a53bc48be
YooHaJin <yoohajin.dev@gmail.com>
2024-07-11 17:56:33 +0900

    feat: 30분 주기로 데이터 전송하는 task 생성

---
+++ dags/dags_seoul_api_to_kafka.py
@@ -0,0 +1,19 @@
+import pendulum
+
+from airflow import DAG
+from operators.seoul_api_to_kafka_operator import SeoulApiToKafkaOperator
+
+with DAG(
+        dag_id='dags_seoul_api_to_kafka',
+        schedule='*/30 * * * *',
+        start_date=pendulum.datetime(2024, 7, 11,  tz='Asia/Seoul'),
+        catchup=False
+) as dag:
+    seoul_population = SeoulApiToKafkaOperator(
+        task_id='seoul_population',
+        dataset_name='{{var.value.seoul_population_dataset}}',
+        kafka_config_id='{{var.value.kafka_config_id}}',
+        topic='{{var.value.seoul_population_topic}}',
+        kafka_publish_key='{{var.value.kafka_publish_key}}',
+        current_time='{{data_interval_end.in_timezone("Asia/Seoul") | ts}}'
+    )
